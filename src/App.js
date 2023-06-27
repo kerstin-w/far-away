456dd3ed07fe14639e4c36c5c35ee44b48f29d25
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-06-27 15:54:55 +0200

    feat: Calculate statistics for items. Also add conditional rendering for the message

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -53,7 +53,6 @@ function Form({ onAddItems }) {
     if (!description) return;
 
     const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log("newItem", newItem);
 
     onAddItems(newItem);
 
@@ -118,10 +117,25 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some item to your packing list 🚀</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>🧳 You have X items on your list, and you already packed X (X%)</em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go! ✈️"
+          : `🧳 You have ${numItems} items on your list, and you already packed
+        ${numPacked} (${percentage}%).`}
+      </em>
     </footer>
   );
 }
